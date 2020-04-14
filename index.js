548c2055f5f585aba45f442d4f3de6074a137f2a
unknown <oyundestesi@outlook.com>
2020-04-14 15:41:31 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// npm init ile paket oluşturuyoruz.
+// package.json dosyasında scripts altında key value şeklinde komutları kısaltabiliriz.
+// Ve bu komutları komut istemcisinde çalıştırmak için npm run keyName, npm run start
+
+// Eğer node_modules ve package.lock.json dosyalarını silersek
+// Modüller package.json'a bağlı olduğundan node ile npm install yazarsak klasörler gelecektir.
